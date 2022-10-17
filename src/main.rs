b381e4152b87741b8f4dc0645e9c7fcf1aee7335
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2022-10-18 01:06:06 +0900

    draw methanol

--- src/main.rs
+++ src/main.rs
@@ -51,6 +51,50 @@ fn filter_color(color: &Color) -> Color {
     }
 }
 
+fn oxygen(x: f64, y: f64, z: f64) -> Box<dyn Hittable> {
+    Box::new(Sphere {
+        center: Point3 { x, y, z },
+        radius: 0.3,
+        material: Box::new(Glass {
+            eta: 1.5,
+            albedo: Attenuation {
+                r: 0.9,
+                g: 0.5,
+                b: 0.5,
+            },
+        }),
+    })
+}
+
+fn carbon(x: f64, y: f64, z: f64) -> Box<dyn Hittable> {
+    Box::new(Sphere {
+        center: Point3 { x, y, z },
+        radius: 0.35,
+        material: Box::new(Metal {
+            albedo: Attenuation {
+                r: 0.5,
+                g: 0.5,
+                b: 0.5,
+            },
+            fuzz: 0.1,
+        }),
+    })
+}
+
+fn hydrogen(x: f64, y: f64, z: f64) -> Box<dyn Hittable> {
+    Box::new(Sphere {
+        center: Point3 { x, y, z },
+        radius: 0.25,
+        material: Box::new(Lambertian {
+            albedo: Attenuation {
+                r: 0.8,
+                g: 0.8,
+                b: 0.9,
+            },
+        }),
+    })
+}
+
 fn main() {
     // Constants for the image:
     let aspect_ratio: f64 = 16.0 / 9.0;
@@ -61,7 +105,7 @@ fn main() {
     let origin = Point3 {
         x: 0.,
         y: 0.,
-        z: 0.,
+        z: 0.5,
     };
     let look_in = Vec3 {
         x: 0.,
@@ -80,59 +124,61 @@ fn main() {
     let camera = Camera::new(origin, look_in, view_up, vertical_fov_radian, aspect_ratio);
 
     // Constants for antialiasing:
-    let num_samples_per_pixel = 10;
+    let num_samples_per_pixel = 100;
 
     // Constants for diffusion:
     let max_diffusion_depth = 10;
 
     // Hittable objects:
-    let sphere1 = Sphere {
-        center: Point3 {
-            x: -1.,
-            y: 0.,
-            z: -1.,
-        },
-        radius: 0.5,
-        material: Box::new(Lambertian {
-            albedo: Attenuation {
-                r: 0.8,
-                g: 0.5,
-                b: 0.5,
+    /*
+        let sphere1 = Sphere {
+            center: Point3 {
+                x: -1.,
+                y: 0.,
+                z: -1.,
             },
-        }),
-    };
-    let sphere2 = Sphere {
-        center: Point3 {
-            x: 1.,
-            y: 0.,
-            z: -1.,
-        },
-        radius: 0.5,
-        material: Box::new(Metal {
-            albedo: Attenuation {
-                r: 0.5,
-                g: 0.5,
-                b: 0.5,
+            radius: 0.5,
+            material: Box::new(Lambertian {
+                albedo: Attenuation {
+                    r: 0.8,
+                    g: 0.5,
+                    b: 0.5,
+                },
+            }),
+        };
+        let sphere2 = Sphere {
+            center: Point3 {
+                x: 1.,
+                y: 0.,
+                z: -1.,
             },
-            fuzz: 0.3,
-        }),
-    };
-    let sphere3 = Sphere {
-        center: Point3 {
-            x: 0.,
-            y: 0.,
-            z: -1.,
-        },
-        radius: 0.5,
-        material: Box::new(Glass {
-            eta: 1.5,
-            albedo: Attenuation {
-                r: 0.9,
-                g: 0.9,
-                b: 0.9,
+            radius: 0.5,
+            material: Box::new(Metal {
+                albedo: Attenuation {
+                    r: 0.5,
+                    g: 0.5,
+                    b: 0.5,
+                },
+                fuzz: 0.3,
+            }),
+        };
+        let sphere3 = Sphere {
+            center: Point3 {
+                x: 0.,
+                y: 0.,
+                z: -1.,
             },
-        }),
-    };
+            radius: 0.5,
+            material: Box::new(Glass {
+                eta: 1.5,
+                albedo: Attenuation {
+                    r: 0.9,
+                    g: 0.9,
+                    b: 0.9,
+                },
+            }),
+        };
+    */
     let ground = Sphere {
         center: Point3 {
             x: 0.,
@@ -143,16 +189,27 @@ fn main() {
         material: Box::new(Lambertian {
             albedo: Attenuation {
                 r: 0.2,
-                g: 0.6,
-                b: 0.4,
+                g: 0.4,
+                b: 0.2,
             },
         }),
     };
+    let (x1, y1, z1) = (0f64, 0f64, -1f64);
+    let len_oh = 0.11;
+    let len_ch = 0.14;
+    let len_co = 0.2;
     let hittable_list = HittableList {
         members: vec![
-            Box::new(sphere1),
-            Box::new(sphere2),
-            Box::new(sphere3),
+            carbon(x1, y1, z1),
+            oxygen(x1 + len_co, y1 + len_co, z1 + len_co),
+            hydrogen(
+                x1 + len_co + len_oh,
+                y1 + len_co - len_oh,
+                z1 + len_co + len_oh,
+            ),
+            hydrogen(x1 + len_ch, y1 - len_ch, z1 - len_ch),
+            hydrogen(x1 - len_ch, y1 - len_ch, z1 + len_ch),
+            hydrogen(x1 - len_ch, y1 + len_ch, z1 - len_ch),
             Box::new(ground),
         ],
     };
